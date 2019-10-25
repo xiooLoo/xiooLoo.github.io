13303ecbed25cce61dec32a36e6a0ec6c77a8044
YangJinpeng <yangjinpeng@camelotchina.com>
2019-10-25 16:27:25 +0800

    Updates

---
+++ precache-manifest.3e243a7403d581f68afb18dec4ea8f4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "471bbd68aebf07e7d4ad77c4f98d1a8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca24b899b20cfbe9ac94",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "947abe3e4a62357c714a",
+    "url": "/static/js/2.017bb613.chunk.js"
+  },
+  {
+    "revision": "ca24b899b20cfbe9ac94",
+    "url": "/static/js/main.74a392ca.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
